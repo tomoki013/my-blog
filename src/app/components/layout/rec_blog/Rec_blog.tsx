f3caa7d7604b67d77c79ac9fe373eaea8068e9dd
tomoki013 <tomoki_ttttt@icloud.com>
2024-11-22 02:56:33 +0900

    add

---
+++ src/app/components/layout/rec_blog/Rec_blog.tsx
@@ -0,0 +1,27 @@
+import React, { useState, useEffect, useCallback } from "react";
+import styles from "./Rec_blog.module.css";
+import Link from "next/link";
+
+const Rec_blog: React.FC = () => {
+    return (
+        <aside>
+
+            {/* 最新のブログ*/}
+            <div>
+                <h2>最新のブログ</h2>
+                <ul>
+                    {/* 最新のブログをここに表示*/}
+                </ul>
+            </div>
+
+            {/* おすすめのブログ*/}
+            <div>
+                <h2>おすすめのブログ</h2>
+                <ul>
+                    {/* おすすめのブログをここに表示*/}
+                </ul>
+            </div>
+
+        </aside>
+    )
+}
