6eec203cef6c5743f2645247d3f7a7e8d6287117
tomoki013 <tomoki_ttttt@icloud.com>
2024-11-27 06:18:59 +0900

    プロフィールコンポーネントを作成しました

---
+++ src/app/components/layout/profile/Profile.tsx
@@ -0,0 +1,20 @@
+import styles from "@/app/components/layout/profile/Profile.module.css";
+import Image from "next/image";
+
+export default function Profile() {
+    return (
+        <section className={styles.profile}>
+            <Image
+                src="/images/diary/Introduce/introduce.jpg"
+                alt="プロフィール写真"
+                width={100}
+                height={100}
+                className={styles.profile_img}
+            />
+            <h3>プロフィール</h3><hr />
+            <p>兵庫県神戸市出身、京都府京都市在住の大学生</p>
+            <p>大学に入ってから旅行にハマりこれまでに全国、そして世界を旅する</p>
+            <p>現在、HTML,CSS,JavaScriptを勉強中でアウトプットのためにブログを始める</p>
+        </section>
+    );
+};
